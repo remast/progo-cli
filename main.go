ef9e79bb9f5e7e0ade64d9fe3a67d596d84ff8ce
Jan Stamer <914776+remast@users.noreply.github.com>
2024-10-13 11:13:54 +0200

    Added json

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
@@ -12,17 +14,25 @@ func main() {
 	// 1. count Flag definieren
 	var count = flag.Int("count", 1, "Count of proverbs to print.")
 	var verbose = flag.Bool("verbose", false, "verbose output")
+	var jsonFormat bool
+	flag.BoolVar(&jsonFormat, "json", false, "Count of proverbs to print.")
 
 	// 2. Flags parsen
 	flag.Parse()
 
 	if *verbose {
-		fmt.Fprintf(os.Stdout, "Printing %v proverbs:\n", *count)
+		fmt.Fprintf(os.Stdout, "Printing %v proverbs:\n", count)
 	}
 
 	// 3. Gewünschte Anzahl Proverbs ausgeben
 	for i := 0; i < *count; i++ {
-		fmt.Println(proverbs.Random().Saying)
+		if jsonFormat {
+			writer := bytes.NewBufferString("")
+			json.NewEncoder(writer).Encode(proverbs.Random())
+			fmt.Println(writer)
+		} else {
+			fmt.Println(proverbs.Random().Saying)
+		}
 	}
 
 }
