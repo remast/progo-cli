dc5d773cae1e5393d4d7df0f0032ba5f0592e698
Jan Stamer <914776+remast@users.noreply.github.com>
2024-10-13 16:55:25 +0200

    x

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 
 	flag "github.com/spf13/pflag"
@@ -10,20 +13,35 @@ import (
 )
 
 func main() {
-	// 1. count Flag definieren
-	var count = flag.IntP("count", "c", 1, "Count of proverbs to print.")
-	var verbose = flag.BoolP("verbose", "v", false, "verbose output")
+	// 1. Flags definieren
+	var count int
+	flag.IntVarP(&count, "c", "count", 1, "Count of proverbs to print.")
+
+	var verbose bool
+	flag.BoolVarP(&verbose, "v", "verbose", false, "verbose output")
+
+	var jsonFormat bool
+	flag.BoolVarP(&jsonFormat, "j", "json", false, "Count of proverbs to print.")
 
 	// 2. Flags parsen
 	flag.Parse()
 
-	if *verbose {
-		fmt.Fprintf(os.Stdout, "Printing %v proverbs:\n", *count)
+	if verbose {
+		fmt.Fprintf(os.Stdout, "Printing %v proverbs:\n", count)
 	}
 
 	// 3. Gewünschte Anzahl Proverbs ausgeben
-	for i := 0; i < *count; i++ {
-		fmt.Println(proverbs.Random().Saying)
+	for i := 0; i < count; i++ {
+		if jsonFormat {
+			writer := bytes.NewBufferString("")
+			err := json.NewEncoder(writer).Encode(proverbs.Random())
+			if err != nil {
+				log.Fatalf("Could not encode json (%v)", err)
+			}
+			fmt.Println(writer)
+		} else {
+			fmt.Println(proverbs.Random().Saying)
+		}
 	}
 
 }
